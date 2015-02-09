096dcf77f88429c6ad8034cc8dfc064efc1301f9
Allyson Schrader <amschrader@gmail.com>
2015-02-08 23:46:16 -0800

    add readme, remove test stub

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ravelry Api Client
+
+A client for [Ravelry](http://www.ravelry.com)
+
+### Setting up defualt auth keys and secret
+
+Once you have a Ravelry api key/secret you can set them up by running metoer with a settings file
+
+```
+meteor --settings settings.json
+```
+
+
+settings.json
+```
+{
+  "ravelryKey": "1234",
+  "ravelrySecret": "abcd"
+}
+```
--- package.js
+++ package.js
@@ -5,12 +5,6 @@ Package.describe({
   git: "https://github.com/amschrader/meteor-ravelry-api.git"
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('amschrader:ravelry');
-  api.addFiles('ravelry-tests.js');
-});
-
 Package.onUse(function (api, where) {
   api.use('oauth1@1.1.2', ['client', 'server']);
   api.add_files(['ravelry.js'], 'server');
